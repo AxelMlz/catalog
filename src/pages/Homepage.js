4efba7a897ab6e64967df04a3a6c9f08055bd8d2
AxelMlz <axel.fm.melezan@gmail.com>
2022-02-15 20:59:30 +0100

    movieDisplay

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,19 +1,21 @@
 import React from "react"
 import {Link} from 'react-router-dom';
 import movies from "../movies";
-
+import Movie from "../pages/Movie"
 
 class Homepage extends React.Component {
   render() {
     return (
-        <div> 
-        <p>Homepage</p>
-        {movies.map(movie => (
-            movie.title
-        )) }
-        </div>
+        <div>
+            <p>Homepage</p>
+            <nav> 
+            {movies.map(movie => {
+                return <li className = "movieLink" > <Link to={`../movies/${movie.id}`} component={Movie}>{movie.title}</Link></li>
+            })}
+         
+            </nav>
+        </div> 
     )
-  }
 }
-
+}
 export default Homepage;
---
+++ src/pages/Movie.js
@@ -0,0 +1,25 @@
+import React from "react"
+import {Link} from 'react-router-dom';
+import movies from "../movies";
+
+
+
+class Movie extends React.Component {
+    constructor(){
+        super()
+        this.state={
+            id:"",
+            
+        }
+        
+    }
+    render() {
+        return (
+            <div>
+             
+            </div> 
+        )
+    }
+}
+
+export default Movie;
